fda27242b58834fb314c40f7e7ea647a8cb3daee
DJangoo00 <dalgramdo@gmail.com>
2022-11-11 19:00:01 -0500

    incluido el soporte para firebase para el inicio de sesion

--- app/src/main/java/com/example/restaurante/MainActivity.kt
+++ app/src/main/java/com/example/restaurante/MainActivity.kt
@@ -4,19 +4,41 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.widget.Toast
+import com.example.restaurante.databinding.ActivityMainBinding
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-    fun ingresar(btningreso:View){
-        val llamado=Intent(this,PedidosActivity::class.java)
-        startActivity(llamado)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val db:FirebaseFirestore=FirebaseFirestore.getInstance()
+
+        binding.btningresar.setOnClickListener{
+            var datos = ""
+            db.collection("usuarios")
+                .get()
+                .addOnSuccessListener { resultado ->
+                    for (documento in resultado){
+                        datos += "${documento.id}:${documento.data}\n"
+                    }
+                    val menu=Intent(this,MenuActivity::class.java)
+                    startActivity(menu)
+                }
+                .addOnFailureListener{
+                        excepcion->
+                    Toast.makeText(this, "Error al iniciar sesión", Toast.LENGTH_LONG).show()
+
+                }
+        }
     }
 
 
